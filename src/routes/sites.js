90b10e3cdf69edfa5ed8e02d7791529fba64fd92
buiquangson123 <buiquangson19091998@gmail.com>
2021-05-26 09:13:48 +0700

    update sites.js

--- src/routes/sites.js
+++ src/routes/sites.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const siteControllers = require('../app/controllers/siteControllers');
 
-router.use('/search', siteControllers.search);
-router.use('/', siteControllers.home);
+                router.use('/search', siteControllers.search);
+                router.use('/', siteControllers.home);
 
 module.exports = router;
