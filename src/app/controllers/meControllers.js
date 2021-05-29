21975489ec86ff57f95b03f14a90409f78b10958
buiquangson123 <buiquangson19091998@gmail.com>
2021-05-29 08:35:06 +0700

    count delete

--- src/app/controllers/meControllers.js
+++ src/app/controllers/meControllers.js
@@ -6,11 +6,27 @@ class MeControllers {
 
     //[GET]/me/stored/courses
     storedCourses(req, res, next) {
-        Course.find({}).lean()
-            .then( courses =>  res.render('me/stored-courses', {        
-                courses,
-            } ))
-            .catch(next);
+
+        Promise.all([Course.find({}).lean(), Course.findDeleted({}).lean()])
+            .then(([courses, coursess]) =>{
+                var deleteCountNew = 0;
+                coursess.map(course => {
+                    if(course.deleted === true){
+                        deleteCountNew ++;
+                    } 
+                });
+                res.render('me/stored-courses', {
+                    deleteCountNew,
+                    courses,
+                })
+            })
+            .catch(next)
+
+        // Course.find({}).lean()
+        //     .then( courses =>  res.render('me/stored-courses', {        
+        //         courses,
+        //     } ))
+        //     .catch(next);
     }
 
     //[GET]/me/trash/courses
